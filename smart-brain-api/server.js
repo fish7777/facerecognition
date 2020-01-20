90536b6c4eff622a2e140af861c870eec9f1a1e5
fish7777 <33596464+fish7777@users.noreply.github.com>
2020-01-20 12:16:38 +0100

    login works now
    login works now

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
 
@@ -13,7 +15,7 @@ const database = {
 		id: '123',
 		name: 'John',
 		email: 'john@gmail.com',
-		password: 'cookies',
+		password:'cookies',
 		entries: 0,
 		joined: new Date()
 
@@ -22,13 +24,20 @@ const database = {
 		id: '124',
 		name: 'Sally',
 		email: 'Sally@gmail.com',
-		password: 'bananas',
 		entries: 0,
 		joined: new Date()
 
 	}
 
 
+	],
+	login:[
+	{
+
+		id:'987',
+		hash:'',
+		email:'john@gmail.com'
+	}
 	]
 
 }
@@ -69,6 +78,43 @@ app.post("/register", (req, res)=>{
 
 })
 
+app.get('/profile/:id', (req, res) =>{
+
+	const {id} = req.params;
+	let found = false;
+
+	database.users.forEach(user => {
+		if(user.id===id){
+			found = true;
+			return res.json(user);
+		}
+
+	})
+	if(!found){
+		res.status(400).json('User not found');
+	}
+
+})
+
+app.put('/image', (req, res)=>{
+
+	const {id} = req.body;
+	let found = false;
+
+	database.users.forEach(user => {
+		if(user.id===id){
+			found = true;
+			user.entries++
+			return res.json(user.entries);
+		}
+
+	})
+	if(!found){
+		res.status(400).json('User not found');
+	}
+
+})
+
 app.listen(3000, ()=> {
 
 console.log('app is running on port 3000')
@@ -78,6 +124,16 @@ console.log('app is running on port 3000')
 })
 
 
+//bcrypt.hash("bacon", null, null, function(err, hash) {
+    // Store hash in your password DB.
+//});
+// Load hash from your password DB.
+//bcrypt.compare("bacon", hash, function(err, res) {
+    // res == true
+//});
+//bcrypt.compare("veggies", hash, function(err, res) {
+    // res = false
+//});
 
 /*
 	/ -----> res = this is working
--- smart-brain/src/components/SignIn/Signin.js
+++ smart-brain/src/components/SignIn/Signin.js
@@ -1,8 +1,53 @@
 import React from 'react';
 
-const Signin = ({onRouteChange}) => {
+class Signin extends React.Component {
 
-	return (
+constructor(){
+	super()
+	this.state = {
+		signInEmail:'',
+		signPassword:''
+	}
+}
+
+onEmailChange = (event) => {
+
+	this.setState({signInEmail: event.target.value})
+
+}	
+onPasswordChange = (event) => {
+
+	this.setState({signPassword: event.target.value})
+
+}
+
+onSubmitSignIn = () => {
+
+	fetch('http://localhost:3000/signin', {
+		method: 'post',
+		headers: {'Content-Type': 'application/json'},
+		body: JSON.stringify({
+			email: this.state.signInEmail,
+			password: this.state.signPassword
+		})
+	})
+	.then(response => response.json())
+	.then(data => {
+		if (data === 'Success') {
+
+		this.props.onRouteChange('home')
+
+		}
+
+	})
+
+}
+
+render() {
+
+	const {onRouteChange} = this.props;
+
+return (
 		<article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
 			<main className="pa4 black-80">
  				<div className="measure">
@@ -10,16 +55,18 @@ const Signin = ({onRouteChange}) => {
 			      <legend className="f1 fw6 ph0 mh0">Sign In</legend>
 			      <div className="mt3">
 			        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-			        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
+			        <input onChange={this.onEmailChange} 
+			        className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
 			      </div>
 			      <div className="mv3">
 			        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-			        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
+			        <input onChange={this.onPasswordChange} 
+			        className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
 			      </div>
 			    </fieldset>
 			    <div className="">
 			      <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" 
-			      onClick={() => onRouteChange('home')}/>
+			      onClick={this.onSubmitSignIn}/>
 			    </div>
 			    <div className="lh-copy mt3">
 			      <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
@@ -31,6 +78,15 @@ const Signin = ({onRouteChange}) => {
 
 		);
 
+
+
 }
+
+
+
+}
+
+	
+
 
 export default Signin;
